8dcb899d40eb6317960c902b574699a8dcdbd5ae
Nishant <nishu.shrivastava23@gmail.com>
2021-08-12 19:36:50 +0530

    Wireframeing

---
+++ student/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from student import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('index', views.index, name='home'),
+    path('upload', views.upload, name='upload'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('report', views.report, name='report'),
+
+]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def upload(request):
+    return render(request, 'upload.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def report(request):
+    return render(request, 'report.html')
