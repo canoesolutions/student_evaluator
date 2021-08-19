ffcfe8b6249593d132c0489f8cfcbb81374703f0
Nishant <nishu.shrivastava23@gmail.com>
2021-08-19 17:13:11 +0530

    Minor Updates

--- student/models.py
+++ student/models.py
@@ -1,21 +1,24 @@
-from enum import unique
+from enum import auto, unique
 from django.db import models
+from datetime import datetime, date
 
 # Create your models here.
+
 class Student_Details(models.Model):
     student_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=30)
-    college_name = models.CharField(max_length=50)
-    branch = models.CharField(max_length=20)
-    year = models.CharField(max_length=20)
-    age = models.IntegerField()
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
     gender = models.CharField(max_length=10)
-    mobile_num = models.CharField(max_length=10, unique=True)
+    age = models.IntegerField()
+    college_name = models.CharField(max_length=50)
+    student_year = models.CharField(max_length=20)
+    branch_name = models.CharField(max_length=20)    
+    mobile_number = models.CharField(max_length=10, unique=True)
     email_id = models.EmailField(max_length=254, unique=True)
     hometown = models.CharField(max_length=20)
+    test_date = models.DateField(blank=True, null=True)
     
-
     def __str__(self):
         return self.first_name
     
